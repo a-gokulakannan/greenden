17e3abe20ebedd8bf296e516610c95b08f8522ec
a-gokulakannan <gokulakannan.ap@gmail.com>
2024-04-11 20:48:05 +0530

    Added Search funtionality

--- product.js
+++ product.js
@@ -10,3 +10,25 @@ menu.addEventListener("click", function () {
 closeIcon.addEventListener("click", function () {
     sideNav.style.right = "-50%"
 })
+
+//Product search
+
+var prodContainer = document.getElementById("product-container")
+var searchbox = document.getElementById("searchbox")
+
+var productList = prodContainer.querySelectorAll("div")
+
+searchbox.addEventListener("keyup", function () {
+    var enterValue = event.target.value.toUpperCase()
+
+    for (i = 0; i < productList.length; i++) {
+        var productName = productList[i].querySelector("h1").textContent
+        
+        if (productName.toUpperCase().indexOf(enterValue) < 0) {
+            productList[i].style.display = "none"
+        } else {
+            productList[i].style.display = "block"
+        }
+
+    }
+})
